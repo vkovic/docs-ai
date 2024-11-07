6632932dc87e67347f9db64c3c630fda6920ce2c
Vladimir Ković <vlada.kovic@gmail.com>
2024-11-07 01:04:41 +0100

    Auto start jupyter notebook server

--- start.sh
+++ start.sh
@@ -16,6 +16,9 @@ mkdir -p /tmp/notebook
 #
 #notebook --notebook-dir=/app/.notebook/ --no-browser --allow-root
 
+# Jupyter Python Notebook
+python -m notebook --notebook-dir=/tmp/notebook/ --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.port_retries=0 --NotebookApp.allow_origin='*' --NotebookApp.ip='0.0.0.0' --port=54321 --no-browser --allow-root > /proc/1/fd/1 2>&1 &
+
 # FastAPI
 fastapi dev src/main.py --host 0.0.0.0 --port 80 --reload &
 
